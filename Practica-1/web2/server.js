512746053ce5176dbba1afb453e448d4ec875000
Irene Vega Lago <ivega@delta33.aulas.gsyc.urjc.es>
2019-02-04 12:55:56 +0100

    New

--- Practica-1/web2/server.js
+++ Practica-1/web2/server.js
@@ -1,10 +1,22 @@
 var http = require('http');
+var fs = require('fs');
 
 console.log("Arrancando servidor...")
 
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-  console.log("Peticion atendida")
+  fs.readFile('tienda.html', function(err, data) {
+    console.log("Recurso soliditado (URL): " + req.url);
+    console.log("Peticion atendida");
+    if (req.url.split('.')[1] == 'css'){
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.write(data);
+        return res.end();
+    }else if (req.url.split('.')[1] == 'jpg'){
+      fs.readFile('peaky.jpg', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/jpg'});
+      });
+    };
+    res.end(data);
+  });
 }).listen(8080);
