14537447480b2c85526db8d7d587960fa1b44cb5
Irene Vega Lago <ivega@alpha25.aulas.gsyc.urjc.es>
2019-04-09 19:07:35 +0200

    nuevo

--- Practica-3/client-2.js
+++ Practica-3/client-2.js
@@ -1,7 +1,7 @@
 function main()
 {
   //-- Traza de prueba
-  console.log("Hola!")
+  console.log("Client!")
 
   //-- Obtener el botón de VER del DOM
   var ver = document.getElementById('ver')
@@ -10,43 +10,44 @@ function main()
   var resultado = document.getElementById('resultado');
 
   //-- Cuando el usuario aprieta el botón de ver los productos
-  ver.onclick = ()=>{
+  ver.onclick = () => {
 
     //-- Crear objeto para hacer peticiones AJAX
     m = new XMLHttpRequest();
 
     //-- Configurar la petición
-        m.open("GET","datos.json", true);
+    m.open("GET", "datos.json", true);
 
     //-- Cuando la haya alguna noticia sobre la peticion
     //-- ejecuta este código
-    m.onreadystatechange=function(){
-       //-- Petición enviada y recibida. Todo OK!
-       if (m.readyState==4 && m.status==200){
+    m.onreadystatechange = function () {
+      //-- Petición enviada y recibida. Todo OK!
+      if (m.readyState == 4 && m.status == 200) {
 
-         //-- La respuesta es un objeto JSON
-         var o = JSON.parse(m.responseText)
-
+        //-- La respuesta es un objeto JSON
+        var o = JSON.parse(m.responseText)
+        console.log(o);
+        
          //-- Borrar el resultado anterior que hubiese en el párrafo
          //-- de resultado
          resultado.innerHTML = "";
 
-         //--Recorrer los productos del objeto JSON
-         for (i=0; i < o.productos.length; i++) {
+        //  //--Recorrer los productos del objeto JSON
+        //  for (i=0; i < o.Camisetas.length; i++) {
 
-           //-- Añadir cada producto al párrafo de visualización
-           resultado.innerHTML += o.productos.name[i];
+        //    //-- Añadir cada producto al párrafo de visualización
+        //    resultado.innerHTML += o.Camisetas.name[i];
 
-           //-- Separamos los productos por ',''
-           if (i<o.productos.length-1) {
-             resultado.innerHTML += ', ';
-           }
-         }
+        //    //-- Separamos los productos por ',''
+        //    if (i<o.Camisetas.length-1) {
+        //      resultado.innerHTML += ', ';
+        //    }
+        //  }
+          }
        }
-     }
 
-     //-- Enviar la petición!
-     m.send();
+    //  //-- Enviar la petición!
+         m.send();
   }
 
 }
