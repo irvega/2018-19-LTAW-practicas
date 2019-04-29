8bdb9de55d466c2223554cd327e8153c191e94ad
irvega <irene@machine.com>
2019-04-29 09:42:04 +0200

    mon

--- Practica-4/chat-client.js
+++ Practica-4/chat-client.js
@@ -1,23 +1,18 @@
 function main() {
-  console.log("Hola!!!!-------------")
+  console.log("Client!")
 
   //-- Crear el websocket
   var socket = io();
-
   //-- Obtener los elementos de interfaz:
 
   //-- Boton de envio de mensaje
   var send = document.getElementById('send')
-
   //-- Parrafo para mostrar mensajes recibidos
   var display = document.getElementById('display')
-
   //-- Caja con el mensaje a enviar
   var msg = document.getElementById("msg")
-
   //-- Cuando se aprieta el botón de enviar...
   send.onclick = () => {
-
     //-- Enviar el mensaje, con el evento "new_message"
     socket.emit('new_message', msg.value);
 
@@ -25,8 +20,7 @@ function main() {
     console.log("Mensaje emitido")
   }
 
-  //-- Cuando se reciba un mensaje del servidor se muestra
-  //-- en el párrafo
+  //-- Cuando se reciba un mensaje del servidor se muestra en el párrafo
   socket.on('new_message', msg => {
     display.innerHTML = msg;
   });
--- Practica-4/test.js
+++ Practica-4/test.js
@@ -2,17 +2,39 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var user = 0;
 //--Servir la pagina principal
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-  console.log("Página principal2: /")
+  console.log(": /")
 });
 
 //-- Servir el cliente javascript
 app.get('/chat-client.js', function(req, res){
   res.sendFile(__dirname + '/chat-client.js');
-  console.log("Fichero js solicituado")
+  console.log("Client solicituado")
 });
+
+app.get('/help', function(req, res){
+  res.send('Comandos soportados:' + '\n\n' + '/list: Devolverá el número de usuarios conectados\n /hello: El servidor nos devolverá el saludo\n /date: Nos devolverá la fecha\n')
+});
+
+app.get('/list', function(req, res){
+  res.send('Número de usuarios conectados: ' + user.toString())
+});
+
+app.get('/hello', function(req, res){
+  res.send('Hello, whats up! ')
+});
+app.get('/date', function(req, res){
+  var f = new Date();
+  //res.send('La fecha actual es: ' + f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
+  var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+  var diasSemana = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
+  res.send('La fecha actual es: ' + diasSemana[f.getDay()] + ", " + f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear());
+
+});
+
 
 //-- Lanzar el servidor
 http.listen(3000, function(){
@@ -23,19 +45,25 @@ http.listen(3000, function(){
 //-- Un nuevo cliente se ha conectado!
 io.on('connection', function(socket){
   console.log('--> Usuario conectado!');
-
+  user= user + 1;
+  // Enviar saludo al que se conecta
+  //socket.broadcast.emit('hi man');
+  
+   msg = 'Nuevo usuario conectado!';
+   io.emit('new_message', msg);
   //-- Detectar si el usuario se ha desconectado
   socket.on('disconnect', function(){
     console.log('--> Usuario Desconectado');
+    user= user - 1;
   });
+
+  // sending to individual socketid (private message)
+  // io.to(`${socketId}`).emit('hey', 'Welcome');
 
   //-- Detectar si se ha recibido un mensaje del cliente
    socket.on('new_message', msg => {
-
    //-- Notificarlo en la consola del servidor
    console.log("Mensaje recibido: " + msg)
-   // sending to individual socketid (private message)
-   io.to(`${socketId}`).emit('hey', 'Welcome');
    //-- Emitir un mensaje a todos los clientes conectados
    io.emit('new_message', msg);
  })
